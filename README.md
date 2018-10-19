87c611ef9dff895a9394c516bdf4fe7ae9f1f44b
rob <roblitvin@me.com>
2018-10-19 15:16:41 -0500

    first state session commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# spark-streaming-state
+
+### Using spark to stream a socket from text file to track ip and session length
---
+++ src/us/rlit/streaming/state/Utilities.scala
@@ -0,0 +1,28 @@
+package us.rlit.streaming.state
+
+import java.util.regex.Pattern
+
+object Utilities {
+  /** Makes sure only ERROR messages get logged to avoid log spam. */
+  def setupLogging() = {
+    import org.apache.log4j.{Level, Logger}
+    val rootLogger = Logger.getRootLogger()
+    rootLogger.setLevel(Level.ERROR)
+  }
+
+  /** Retrieves a regex Pattern for parsing Apache access logs. */
+  def apacheLogPattern(): Pattern = {
+    val ddd = "\\d{1,3}"
+    val ip = s"($ddd\\.$ddd\\.$ddd\\.$ddd)?"
+    val client = "(\\S+)"
+    val user = "(\\S+)"
+    val dateTime = "(\\[.+?\\])"
+    val request = "\"(.*?)\""
+    val status = "(\\d{3})"
+    val bytes = "(\\S+)"
+    val referer = "\"(.*?)\""
+    val agent = "\"(.*?)\""
+    val regex = s"$ip $client $user $dateTime $request $status $bytes $referer $agent"
+    Pattern.compile(regex)
+  }
+}
